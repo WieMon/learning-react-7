0b2fb01df17beed15fd8a113d027b69d17dda0b8
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-05-15 11:38:58 +0200

    Add layout components Add css to App

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import '../styles/App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
- 
-    </div>
+    <Router>
+      <div className='app'>
+          <header>
+            {<Header />}
+          </header>
+          <main>
+            <aside>
+              {<Navigation />}
+            </aside>
+            <section className='page'>
+              {<Page />}
+            </section>
+          </main>
+          <footer>{<Footer />}</footer>
+        </div>
+    </Router>
+   
   );
 }
 
