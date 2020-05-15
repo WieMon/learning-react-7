9a8cba90a589df569417ac9a8f70091048957048
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-05-15 15:32:13 +0200

    Add Contact

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import '../styles/ContactPage.css';
+import { Prompt } from 'react-router-dom';
 
-const ContactPage = () => {
-  return ( 
-    <div>Contact</div>
-   );
+class ContactPage extends React.Component {
+
+  state = {
+    value: '',
+    isEmpty: true,
+  }
+
+  handleChange = (e) => {
+    if (e.target.value.length > 0) {
+        this.setState({
+            value: e.target.value,
+            isEmpty: false
+        })
+    } else {
+        this.setState({
+            value: e.target.value,
+            isEmpty: true
+        })
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      value: '',
+      isEmpty: true
+    })
+  }
+
+  render() {
+    return (
+      <div className='contact'>
+        <form onSubmit={this.handleSubmit}>
+          <h3>Contact us</h3>
+          <textarea value={this.state.value} onChange={this.handleChange} placeholder='Please type your message'></textarea>
+          <button>Send</button>
+        </form>
+        <Prompt
+                    when={!this.state.isEmpty}
+                    message="Do you want to leave?"
+                />
+      </div>
+    )
+  }
 }
  
 export default ContactPage;
