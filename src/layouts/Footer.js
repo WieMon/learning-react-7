35f2c3c7591113574d0564cbf4b4d7f1ffeb6963
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-05-16 16:35:14 +0200

    Add Footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import "../styles/Footer.css";
 
-const Footer= () => <div>Footer</div>
+const Footer= () => {
+  return (
+    <div>
+      <h2>Footer</h2>
+      <Route path="/" exact render={(props) => (
+        <p>You are on <span>the homepage.</span></p>
+      )
+      } />
+
+      <Route path="/:page" exact render={(props) => {
+              console.log(props);
+              return (
+                <>
+                  <p>You are on <span>{props.match.params.page}</span></p>
+                  <p>You are on <span>{props.match.url}</span></p>
+                  <p>You are on <span>{props.match.path}</span></p>
+                </>
+              )
+            }} />
+
+      <Route path="/:page/:idProduct" exact render={(props) => {
+              console.log(props);
+              return (
+                <>
+                  <p>You are on <span>{props.match.params.idProduct}</span></p>
+                  <p>You are on <span>{props.match.url}</span></p>
+                  <p>You are on <span>{props.match.path}</span></p>
+                </>
+              )
+            }} />
+    </div>
+  )
+}
  
 export default Footer;
